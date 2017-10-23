9d3045717f480dd2c33be218a1da5a8e96e1a563
Mark van Beek <flipvb@gmail.com>
2017-10-23 21:21:07 +0200

    Add watch container

--- angular-viewport-watch.js
+++ angular-viewport-watch.js
@@ -1,30 +1,76 @@
 "use strict";
 
-(function() {
-    viewportWatch.$inject = [ "scrollMonitor", "$timeout" ];
-    function viewportWatch(scrollMonitor, $timeout) {
+(function () {
+    function viewportWatch() {
         var viewportUpdateTimeout;
+
         function debouncedViewportUpdate() {
-            $timeout.cancel(viewportUpdateTimeout);
-            viewportUpdateTimeout = $timeout(function() {
-                scrollMonitor.update();
+            if (viewportUpdateTimeout) {
+                clearTimeout(viewportUpdateTimeout);
+            }
+
+            viewportUpdateTimeout = setTimeout(function () {
+                window.scrollMonitor.update();
             }, 10);
         }
+
         return {
             restrict: "AE",
-            link: function(scope, element, attr) {
-                var elementWatcher = scrollMonitor.create(element, scope.$eval(attr.viewportWatch || "0"));
+            link: function (scope, element, attr) {
+                var viewportUpdate = debouncedViewportUpdate;
+                var elementWatcher;
+
+                if (attr.watchContainer) {
+                    var container = element[0].closest(attr.watchContainer);
+
+                    var watchContainer;
+                    if (container.$watchContainer) {
+                        watchContainer = container.$watchContainer;
+                    } else {
+                        container.$watchContainer = watchContainer = window.scrollMonitor.createContainer(container);
+
+                        angular.element(container).on('$destroy', function () {
+                            watchContainer.destroy();
+
+                            if (watchContainer.containerWatcher) {
+                                watchContainer.containerWatcher.destroy();
+                            }
+
+                            window.scrollMonitor.update();
+                        });
+                    }
+
+                    viewportUpdate = function() {
+                        if (viewportUpdateTimeout) {
+                            clearTimeout(viewportUpdateTimeout);
+                        }
+
+                        viewportUpdateTimeout = setTimeout(function () {
+                            watchContainer.update();
+                        }, 10);
+                    };
+
+                    elementWatcher = watchContainer.create(element, scope.$eval(attr.viewportWatch || "0"));
+                } else {
+                    elementWatcher = window.scrollMonitor.create(element, $scope.$eval(attr.viewportWatch || "0"));
+                }
+
                 function watchDuringDisable() {
                     this.$$watchersBackup = this.$$watchersBackup || [];
                     this.$$watchers = this.$$watchersBackup;
+
                     var unwatch = this.constructor.prototype.$watch.apply(this, arguments);
+
                     this.$$watchers = null;
+
                     return unwatch;
                 }
+
                 function toggleWatchers(scope, enable) {
                     var digest, current, next = scope;
                     do {
                         current = next;
+
                         if (enable) {
                             if (current.hasOwnProperty("$$watchersBackup")) {
                                 current.$$watchers = current.$$watchersBackup;
@@ -39,7 +85,9 @@
                                 current.$watch = watchDuringDisable;
                             }
                         }
+
                         next = current.$$childHead;
+
                         while (!next && current !== scope) {
                             if (current.$$nextSibling) {
                                 next = current.$$nextSibling;
@@ -48,31 +96,42 @@
                             }
                         }
                     } while (next);
+
                     if (digest) {
                         scope.$digest();
                     }
                 }
+
                 function disableDigest() {
                     toggleWatchers(scope, false);
                 }
+
                 function enableDigest() {
                     toggleWatchers(scope, true);
                 }
+
                 if (!elementWatcher.isInViewport) {
                     scope.$evalAsync(disableDigest);
-                    debouncedViewportUpdate();
+                    viewportUpdate();
                 }
+
                 elementWatcher.enterViewport(enableDigest);
                 elementWatcher.exitViewport(disableDigest);
-                scope.$on("toggleWatchers", function(event, enable) {
+
+                scope.$on("toggleWatchers", function (event, enable) {
                     toggleWatchers(scope, enable);
                 });
-                scope.$on("$destroy", function() {
+
+                scope.$on("$destroy", function () {
                     elementWatcher.destroy();
-                    debouncedViewportUpdate();
+                    viewportUpdate();
                 });
             }
         };
     }
-    angular.module("angularViewportWatch", []).directive("viewportWatch", viewportWatch).value("scrollMonitor", window.scrollMonitor);
+
+    viewportWatch.$inject = [];
+
+    angular.module("angularViewportWatch", [])
+        .directive("viewportWatch", viewportWatch);
 })();
